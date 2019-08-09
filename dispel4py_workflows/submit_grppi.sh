e5e1959871ef876f9869785b58577f7d64aa5f3a
Javier Fernandez Muñoz <javidoc00@gmail.com>
2019-08-09 11:18:48 +0100

    update

--- dispel4py_workflows/submit_grppi.sh
+++ dispel4py_workflows/submit_grppi.sh
@@ -1,18 +1,32 @@
 set -x
 
+  export WORKFLOWS_DIR="/Users/javierfernandezmunoz/EPCC/version buena"
+  #export WORKFLOWS_DIR="/lustre/home/dc010/jfmunoz/test/dispel4py_GrPPI_workflows"
+
+  export NUM_CORES=10
+  export LOAD_SAVE="LOAD"  # [SAVE, LOAD]
+  export OPERATION="ALL"  # [ALL, PREP, XCROSS]
+
   export PYTHONPATH=$PYTHONPATH:.
+  export GRPPI_DIR=${WORKFLOWS_DIR}/grppi_workflows/build/use_cases/tc_cross_correlation/
+  export OUTPUT_DIR=${WORKFLOWS_DIR}/dispel4py_workflows/tc_cross_correlation/OUTPUT/
+  export DISPEL4PY_XCORR_STARTTIME=2019-07-21T06:00:00.000
+  export DISPEL4PY_XCORR_ENDTIME=2019-07-21T08:00:00.000
 
-  export GRPPI_DIR=/lustre/home/dc010/jfmunoz/test/dispel4py_GrPPI_workflows/grppi_workflows/build/use_cases/tc_cross_correlation/
-  export OUTPUT_DIR=/lustre/home/dc010/jfmunoz/test/dispel4py_GrPPI_workflows/dispel4py_workflows/tc_cross_correlation/OUTPUT2/
+  if [ "$LOAD_SAVE" == "SAVE" ]; then
+    rm -rf ./tc_cross_correlation/OUTPUT/SOURCE
+    mkdir  ./tc_cross_correlation/OUTPUT/SOURCE
+  fi
 
-  export DISPEL4PY_XCORR_STARTTIME=2019-08-07T06:00:00.000
-  export DISPEL4PY_XCORR_ENDTIME=2019-08-07T07:00:00.000
+  if [ "$OPERATION" != "XCROSS" ]; then
+    rm -rf ./tc_cross_correlation/OUTPUT/DATA
+    mkdir  ./tc_cross_correlation/OUTPUT/DATA
+    dispel4py multi tc_cross_correlation/filed_prep.py -f tc_cross_correlation/realtime_xcorr_input.jsn -n $NUM_CORES
+  fi
 
-  rm -rf ${OUTPUT_DIR}/DATA
-  rm -rf ${OUTPUT_DIR}/XCORR
-  mkdir ${OUTPUT_DIR}/DATA
-  mkdir ${OUTPUT_DIR}/XCORR
-
-  dispel4py multi tc_cross_correlation/realtime_prep.py -f tc_cross_correlation/realtime_xcorr_input.jsn -n 10
-  #dispel4py multi tc_cross_correlation/realtime_xcorr_mod.py -n 10
-  taskset -c 0-9 ${GRPPI_DIR}/tc_cross_correlation $OUTPUT_DIR $DISPEL4PY_XCORR_STARTTIME tbb
+  if [ "$OPERATION" != "PREP" ]; then
+    rm -rf ./tc_cross_correlation/OUTPUT/XCORR
+    mkdir ./tc_cross_correlation/OUTPUT/XCORR
+    dispel4py multi tc_cross_correlation/realtime_xcorr_mod.py -n $NUM_CORES
+    #taskset -c 0-$(($NUM_CORES-1)) ${GRPPI_DIR}/tc_cross_correlation $OUTPUT_DIR $DISPEL4PY_XCORR_STARTTIME tbb
+  fi
--- dispel4py_workflows/tc_cross_correlation/realtime_xcorr_mod.py
+++ dispel4py_workflows/tc_cross_correlation/realtime_xcorr_mod.py
@@ -16,30 +16,40 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.backends
-from obspy import read
+from obspy import read, UTCDateTime
 
 #ROOT_DIR = './tc_cross_correlation/OUTPUT/'
 ROOT_DIR = os.environ['OUTPUT_DIR']
-#starttime = '2019-07-21T06:00:00.000'
-starttime = os.environ['DISPEL4PY_XCORR_STARTTIME'] 
+
+#starttime='2019-07-21T06:00:00.000'
+starttime = os.environ['DISPEL4PY_XCORR_STARTTIME']
+
+#endtime='2019-07-21T07:00:00.000'
+endtime = os.environ['DISPEL4PY_XCORR_ENDTIME']
+
+t_start = UTCDateTime(starttime)
+t_finish = UTCDateTime(endtime)
 
 class Product(GenericPE):
     def __init__(self):
         GenericPE.__init__(self)
         self._add_output('output', tuple_type=['number'])
     def process(self, inputs):
-        store = []
-        dirs = [x for x in os.listdir(ROOT_DIR + 'DATA/'+ starttime) if not x.startswith('.')]
-        for dir in dirs:
-            files = [x for x in os.listdir(ROOT_DIR+'/DATA/'+ starttime +'/'+ dir) if not x.startswith('.')]
-            for f in files:
-                file=ROOT_DIR+'DATA/'+ starttime + '/'+ dir+'/'+f
-                str1=np.load(file)
-                index = len(store)
-                for i in range(index):
-                    self.write('output',[i, index, store[i], str1])
-               #self.log('The station %s is index %s' %(f,index))
-                store.append(str1)
+        lapse = int((t_finish - t_start) // 3600)
+        for inc in range(0, lapse):
+            store = []
+            t_now = t_start + (inc*3600)
+            dirs = [x for x in os.listdir(ROOT_DIR + 'DATA/'+ t_now.__str__()) if not x.startswith('.')]
+            for dir in dirs:
+                files = [x for x in os.listdir(ROOT_DIR+'/DATA/'+ t_now.__str__() +'/'+ dir) if not x.startswith('.')]
+                for f in files:
+                    file=ROOT_DIR+'DATA/'+ t_now.__str__() + '/'+ dir+'/'+f
+                    str1=np.load(file)
+                    index = len(store)
+                    for i in range(index):
+                        self.write('output',[i, index, store[i], str1, t_now])
+                    #self.log('The station %s is index %s' %(f,index))
+                    store.append(str1)
 
 class Xcorr(IterativePE):
     def __init__(self):
@@ -47,9 +57,10 @@ class Xcorr(IterativePE):
     def _process(self, data):
         str1=data[2]
         str2=data[3]
+        t_now=data[4]
         try:
             xcorr1 = xcorrf_noFFT(str1, str2, 5000)
-            return (data[0], data[1], xcorr1)
+            return (data[0], data[1], xcorr1, t_now)
         except:
             self.log('error in %s_%s xcorr' % (data[0], data[1]))
             self.log(traceback.format_exc())
@@ -62,7 +73,8 @@ class StoreToFile(ConsumerPE):
         self.counter = 0
     def _process(self, data):
         xcorr1 = data[2]
-        directory=ROOT_DIR+'XCORR/'+starttime+'/'+str(data[0])+'_'+str(data[1])
+        t_now=data[3]
+        directory=ROOT_DIR+'XCORR/'+t_now.__str__()+'/'+str(data[0])+'_'+str(data[1])
         if not os.path.exists(directory):
     	    os.makedirs(directory)
         fout = directory+'/%s_%s_%s.out' % (self.filename, data[0], data[1])
@@ -78,7 +90,8 @@ class Plot(ConsumerPE):
         self.filename = filename
     def _process(self, data):
         xcorr1 = data[2]
-        fout = ROOT_DIR+'XCORR/%s_%s_%s.plot.eps' % (self.filename, data[0], data[1])
+        t_now=data[3]
+        fout = ROOT_DIR+'XCORR/'+t_now.__str__()+'/' + '%s_%s_%s.plot.eps' % (self.filename, data[0], data[1])
         sps=4
         plt.plot(np.linspace((-len(xcorr1)+1)/(2*sps),len(xcorr1)/(2*sps),len(xcorr1)), xcorr1)
         plt.xlim(-1000,1000)
